2f6633c58e92b97f600c923e6927efdcee96871c
Fortunejite <fortunejite1234@gmail.com>
2023-09-03 11:08:00 +0100

    updates

--- utils/redis.js
+++ utils/redis.js
@@ -3,10 +3,8 @@ const redis = require('redis');
 
 class RedisClient {
   constructor() {
-    // Create a Redis client
     this.client = redis.createClient();
 
-    // Handle errors and log them to the console
     this.client.on('error', (err) => {
       console.error('Redis Error:', err);
     });
@@ -14,7 +12,6 @@ class RedisClient {
 
   isAlive() {
     try {
-    // Check if the Redis client is connectedtry {
       this.client.ping();
       return true;
     } catch (err) {
@@ -23,11 +20,9 @@ class RedisClient {
   }
 
   async get(key) {
-    // Convert Redis get method to a promise-based function
     const getAsync = promisify(this.client.get).bind(this.client);
 
     try {
-      // Get the value associated with the given key
       return await getAsync(key);
     } catch (error) {
       return null;
@@ -36,7 +31,6 @@ class RedisClient {
 
   async set(key, value, duration) {
     try {
-      // Set the value with an expiration in seconds
       await this.client.setex(key, duration, value);
     } catch (error) {
       console.error('Redis Set Error:', error);
@@ -45,7 +39,6 @@ class RedisClient {
 
   async del(key) {
     try {
-      // Delete the value associated with the given key
       await this.client.del(key);
     } catch (error) {
       console.error('Redis Delete Error:', error);
@@ -53,6 +46,5 @@ class RedisClient {
   }
 }
 
-// Create and export an instance of RedisClient
 const redisClient = new RedisClient();
 module.exports = redisClient;
